b41b8c73b4ec5a852277d03d037807bd9f0cc1d2
ReginaldjSparrow <60196840+ReginaldjSparrow@users.noreply.github.com>
2021-11-17 13:32:51 -0500

    lab 10 Comp
    lab 10

---
+++ client/js/retrieveinfo.js
@@ -0,0 +1,78 @@
+usewhenneeded();
+
+
+function usewhenneeded(){
+$.ajax({
+    url:"http://localhost:3000/getinfo",
+    type: 'get',
+    success: function(responce) {
+        console.log(responce);   
+        var returnData = JSON.parse(responce);
+
+       if(returnData.msg === "SUCCESS") {
+           displaytable(returnData.gameDatabase);
+       } else {
+           console.log(responce)
+       }
+   },
+   error: function(responce) {
+       console.log(responce);
+   }
+});
+}
+
+
+function displaytable(dataarray) {
+    var htmlString = "";
+        for(i=0; i < dataarray.length; i++) {
+            htmlString += "<tr>";
+                htmlString += "<td>";
+                    htmlString += dataarray[i].gameTitle;
+                htmlString += "</td>";
+                htmlString += "<td>";
+                    htmlString += dataarray[i].console;
+                htmlString += "</td>";
+                htmlString += "<td>";
+                    htmlString += dataarray[i].category;
+                htmlString += "</td>";
+                htmlString += "<td>";
+                    htmlString += dataarray[i].esrb;
+                htmlString += "</td>";
+                htmlString += "<td>";
+                    htmlString += dataarray[i].rating;
+                htmlString += "</td>";
+                htmlString += "<td>";
+                    htmlString += dataarray[i].review;
+                htmlString += "</td>";
+                htmlString += "<td>";
+                    htmlString += "<button class='delete-button' data-id='" + dataarray[i].id + "'>Erase</button>"
+                htmlString += "</td>";
+            htmlString += "</tr>";
+        }
+
+        $("#tablebody").html(htmlString);
+        deleteButtonFunc();
+}
+
+function deleteButtonFunc() {
+    $('.delete-button').click(function() {
+        var gameID = this.getAttribute('data-id');
+        $.ajax({
+            url:"http://localhost:3000/getinfo",
+            type: 'delete',
+            data: {id: gameID},
+            success: function(responce) {
+                var returnData = JSON.parse(responce);
+
+            if(returnData.msg === "SUCCESS") {
+                usewhenneeded();
+            } else {
+                console.log(responce)
+            }
+        },
+        error: function(responce) {
+            console.log(responce);
+        }
+    });
+    });
+}
--- src/app.js
+++ src/app.js
@@ -3,22 +3,24 @@ const app = express();
 const path = require("path");
 const bodyParser = require('body-parser');
 
-app.use((req, res, next) => {
-    res.header('Acces-Control-Allow-Origin', '*');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use("/client", express.static(path.resolve(__dirname + "/../client/")));
 
 var server;
-var port = process.env.PORT || process.env.NODE_PORT || 5000;
+var port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 //Router
-
+var router = require("./router.js");
+router(app);
 //Data Processes
-
+var services = require('./services.js');
+services(app);
 //Listen
 server = app.listen(port, function(err) {
     if(err) {
